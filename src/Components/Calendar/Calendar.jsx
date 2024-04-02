fc33bce327403cda0c947c2456b0101e9b99aa16
Rodrigo Grande <96559980+rodrigogz64@users.noreply.github.com>
2024-04-01 20:26:12 -0600

    Update Calendar.jsx

--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -11,23 +11,23 @@ export default function Calendar() {
   const staticEvents = [
     {
       title: 'Car-12 By Client #250',
-      start: '2024-03-05',
-      end: '2024-03-20',
+      start: '2024-04-05',
+      end: '2024-04-20',
     },
     {
       title: 'Car-5 By Client #136',
-      start: '2024-03-15',
-      end: '2024-03-28',
+      start: '2024-04-15',
+      end: '2024-04-28',
     },  
     {
       title: 'Car-9 By Client #3',
-      start: '2024-03-28',
-      end: '2024-03-30',
+      start: '2024-04-28',
+      end: '2024-05-10',
     },
     {
       title: 'Car-15 By Client #9',
-      start: '2024-04-01',
-      end: '2024-04-03',
+      start: '2024-04-25',
+      end: '2024-05-03',
     },
   ];
 
